b5670d526ac07ed3829669177105d1ae8d59ca65
thj <740905172@qq.com>
2018-07-11 13:15:05 +0800

    modify touch method

--- src/components/vue-slide-nav/slide-page-wrapper/slide-page-wrapper.vue
+++ src/components/vue-slide-nav/slide-page-wrapper/slide-page-wrapper.vue
@@ -5,7 +5,9 @@
        @touchmove="pageTouchMove($event)"
        @touchend="pageTouchEnd($event)"
   >
-    <slot/>
+    <div class="page-wrapper" ref="pageWrapper">
+      <slot/>
+    </div>
   </div>
 </template>
 
@@ -17,10 +19,15 @@
         startPageX: 0, // 初始鼠标X轴位置
         startPageY: 0,  // 初始鼠标Y轴位置
         htmlFontSize: 0, // px 转rem
-        offsetX:'',  // 初始偏移X
-        offsetY:'',  // 初始偏移Y
-        differX:0,  // X差值
-        differY:0,  // Y差值
+        offsetX: '',  // 初始偏移X
+        offsetXNum: '',  // 初始偏移X
+        offsetY: '',  // 初始偏移Y
+        offsetYNum: '',  // 初始偏移Y
+        differX: 0,  // X差值
+        differY: 0,  // Y差值
+        isLeft:true,  // 默认左划
+
+
       }
 
     },
@@ -31,35 +38,78 @@
     methods: {
       pageTouchStart(ev) {
         let dom = ev.changedTouches[0];
+        // 获取初始值
         this.startPageX = dom.pageX;
         this.startPageY = dom.pageY;
 
-        let transfrom = this.$refs.slidePageWrapper.style.transform;
+        // 获取当前pageWrapper的偏移
+        let transfrom = this.$refs.pageWrapper.style.transform;
 
-        if (transfrom !== "undefined" && transfrom !== ''){
+        // 如果有偏移那就保存初始偏移位置（带单位） 没有就默认0px
+        if (transfrom !== "undefined" && transfrom !== '') {
           let translateArr = transfrom.match(/\(([^)]*)\)/)[1].split(',');
           this.offsetX = translateArr[0];
+          this.offsetXNum = Number(translateArr[0].split('px')[0]);
           this.offsetY = translateArr[1];
-        }else{
+          this.offsetY = Number(translateArr[1].split('px')[0]);
+
+        } else {
           this.offsetX = '0px';
+          this.offsetXNum = 0;
           this.offsetY = '0px';
+          this.offsetYNum = 0;
         }
       },
+
       pageTouchMove(ev) {
 
-        let slidePageWrapper = this.$refs.slidePageWrapper;
+        let pageWrapper = this.$refs.pageWrapper;
+
+
         let dom = ev.changedTouches[0];
-        let differX = Math.abs(dom.pageX - this.startPageX);
-        let differY = Math.abs(dom.pageY - this.startPageY);
-        console.log(differX/100,differY/100)
-        if (differX/100 > differY/100) {
-          slidePageWrapper.style.transform = `translate(${-differX/this.htmlFontSize}rem,0)`
-        }else{
-          slidePageWrapper.style.transform = `translate(${this.offsetX},${this.offsetY})`
+
+        if (this.startPageX > dom.pageX) {
+          // 右划
+          // 计算滑动距离
+          this.differX = Math.abs(dom.pageX - this.startPageX);
+          this.differY = Math.abs(dom.pageY - this.startPageY);
+          // 设置滑动方向
+          this.isLeft = false;
+        } else {
+          // 左划
+          // 计算滑动距离
+          this.differX = this.startPageX - dom.pageX;
+          this.differY = this.startPageY - dom.pageY;
+          this.isLeft = true;
         }
 
+        // 当滑动距离 X>Y的时候才赋值 一旦X<Y就直接改为初始偏移
+        if (Math.abs(this.differX) / 100 > Math.abs(this.differY) / 100) {
+          pageWrapper.style.transform = `translate(${this.offsetXNum ? (this.offsetXNum + -this.differX) / this.htmlFontSize : -this.differX / this.htmlFontSize}rem,0)`
+        } else {
+          pageWrapper.style.transform = `translate(${this.offsetX},${this.offsetY})`
+        }
       },
       pageTouchEnd(ev) {
+        // 获取屏幕的宽度
+        const WINDOW_WIDTH = document.documentElement.offsetWidth;
+
+        let pageWrapper = this.$refs.pageWrapper;
+
+
+        if (WINDOW_WIDTH / 3 < Math.abs(this.differX)) {
+          if (this.isLeft){
+              // 左划
+            pageWrapper.style.transform = `translate(${this.offsetXNum ? this.offsetXNum + WINDOW_WIDTH : WINDOW_WIDTH}px,0)`;
+
+          }else{
+            // 右划
+            pageWrapper.style.transform = `translate(${this.offsetXNum ? this.offsetXNum + -WINDOW_WIDTH : -WINDOW_WIDTH}px,0)`
+          }
+        } else {
+          pageWrapper.style.transform = `translate(${this.offsetX},${this.offsetY})`
+        }
+
 
       }
     }
@@ -69,6 +119,10 @@
 <style scoped lang="less" rel="stylesheet/less">
   .slide-page-wrapper {
     overflow: hidden;
-    display: flex;
+    position: relative;
+    .page-wrapper {
+      display: flex;
+      transition: transform 150ms ease-in-out;
+    }
   }
 </style>
